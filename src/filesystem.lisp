d012358632011f29a30f38150d4cdbef261adfaa
Mark Fedurin <hitecnologys@gmail.com>
2013-12-12 22:08:19 +0700

    Added storing of *nonce* value

--- src/filesystem.lisp
+++ src/filesystem.lisp
@@ -1,10 +1,11 @@
 (in-package :tradebot)
 
 (defun save-data ()
-  (cl-store:store (list *nonce*)
+  (cl-store:store (list *nonce* *history*)
                   #p"~/Documents/trade_bot.db"))
 
 (defun load-data ()
-  (destructuring-bind (nonce)
+  (destructuring-bind (nonce history)
       (cl-store:restore #p"~/Documents/trade_bot.db")
-    (setf *nonce* nonce)))
+    (setf *nonce* nonce
+          *history* history)))
