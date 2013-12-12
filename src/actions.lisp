6cd57d8db90254ba6b96398af9fa24e3f6dd99f7
Mark Fedurin <hitecnologys@gmail.com>
2013-12-12 22:24:30 +0700

    Implemented function to count types of orders in trades history

--- src/actions.lisp
+++ src/actions.lisp
@@ -15,3 +15,15 @@
                                  "rate" (order-rate order)
                                  "amount" (order-amount order))))
     (setf (order-id order) (jsown:val response "order_id"))))
+
+(defun count-orders-types (orders-history)
+  (loop for order in orders-history
+        if (equal (order-type order) "sell")
+          sum 1 into sell-count
+        else if (equal (order-type order) "buy")
+          sum 1 into buy-count
+        finally (return (list :buy buy-count :sell sell-count))))
+
+(defun get-orders-types-ratio (orders-types-count)
+  (/ (getf orders-types-count :buy)
+     (getf orders-types-count :sell)))
